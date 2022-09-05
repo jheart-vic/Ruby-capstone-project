68e3ec4b298f95497e1817aaca3ef33a4baa8189
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-05 15:26:48 +0100

    Add Main.rb files for our app console

---
+++ main.rb
@@ -0,0 +1,14 @@
+class Main
+  def self.display_options
+    puts 'Please choose an option by entering a number:
+    1 - .
+    2 - .
+    3 - .
+    4 - .
+    5 - .
+    6 - .
+    7 - Exit
+    '
+    Integer(gets.chomp)
+  end
+end
