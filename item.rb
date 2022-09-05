f2cd59e176c84a2cc3fb6ce612b6ece40a0d2b59
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-05 15:26:19 +0100

    Add move_to_achived method in Item class

--- item.rb
+++ item.rb
@@ -1,20 +1,22 @@
 class Item
-  def intialize(author, genre, label, publish_date, source, id = Random.rand(1..1000))
-    @author = author
-    @genre = genre
-    @label = label
+  attr_accessor :author, :source, :genre, :label
+
+  def intialize(publish_date, archived = false, id = Random.rand(1..1000))
     @publish_date = publish_date
-    @source = source
-    @archive = false
+    @archived = archived
   end
   
-  def can_be_archived
-    # Date::strptime(publish_date, "%Y-%m-%d")
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end  
+
+  private
+
+  def can_be_archived?   
     current_date = Date.strptime(publish_date, "%Y-%m-%d")
     calculated_date = current_date - @publish_date
     return true if calculated_date > 10
   
     false
   end
-  private :can_be_archived
 end
