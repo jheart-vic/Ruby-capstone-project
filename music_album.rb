f2344aef9c3654e92be623a186714bf104625bd3
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-05 23:06:40 +0100

    Create MusicAlbum class with constructor

---
+++ music_album.rb
@@ -0,0 +1,9 @@
+require_relative './item'
+
+class MusicAlbum < Item
+  def initialize(publish_date, id = Random.rand(1..1000), on_spotify: false, archived: false)
+    super(publish_date, id, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+end
