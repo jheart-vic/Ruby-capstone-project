ebe346bc97c6594e5334abc81a3798b385c05e34
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-06 00:04:33 +0100

    Implement can_be_archived? method in MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -6,4 +6,11 @@ class MusicAlbum < Item
     @on_spotify = on_spotify
   end
 
+  def can_be_archived?
+    super and @on_spotify
+  end
 end
+
+tu_face = MusicAlbum.new("2015-04-09", rand(1..1000), on_spotify: true)
+
+p tu_face.can_be_archived?
