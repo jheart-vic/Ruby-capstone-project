bc8bb4aa7c836a63bdcb130315e629d5198092ed
jheart-vic <adebowalevictorjheart@gmail.com>
2022-09-06 15:59:48 +0100

    Modify excution, book and create_books files

--- create_book.rb
+++ create_book.rb
@@ -1,3 +1,4 @@
+require 'date'
 require './book'
 require './book_store'
 
@@ -8,13 +9,29 @@ class AddBook
   end
 
   def add_book
-    print 'Publisher:'
+    print 'Publisher:  '
     publisher = gets.chomp.strip.capitalize
     print 'cover_state:  '
     cover_state = gets.chomp.strip.capitalize
     print 'publish_date:  '
-    publish_date = gets.chomp
-    @books << Book.new(publisher, cover_state, publish_date)
-    print('Book created successfully', 'Happy learning\n')
+    check_date = lambda do
+      puts 'publish_date (yyyy-mm-dd):'
+      publish_date = validate_date(gets.chomp.to_s)
+      if publish_date
+        @books << Book.new(publisher, cover_state, publish_date)
+        puts("Book created successfully', 'Happy learning\n")
+      else
+        puts 'Invalid Date or format: please enter correct date'
+        puts "\n"
+        check_date.call
+      end
+    end
+    check_date.call
+  end
+
+  def validate_date(date)
+    Date.parse(date)
+  rescue StandardError
+    false
   end
 end
--- execution.rb
+++ execution.rb
@@ -1,24 +1,29 @@
 require './games/add_game'
 require './create_book'
+require './list_books'
 require './book_store'
 class Execution
-    include BookStore
+  include BookStore
 
-    attr_accessor :game_list
-    def initialize
-        @game_list = []
-        @author_list = []
-        @books = load_books
-        @labels = []
-        @genres = []
-    end
+  attr_accessor :game_list
 
-    def add_game
-        AddGame.new(@game_list).add_game
-    end
+  def initialize
+    @game_list = []
+    @author_list = []
+    @books = load_books
+    @labels = []
+    @genres = []
+  end
 
-    def create_book
-        AddBook.new(@books).add_book
-    end
+  def add_game
+    AddGame.new(@game_list).add_game
+  end
 
+  def create_book
+    AddBook.new(@books).add_book
+  end
+
+  def list_all_books
+    ListBook.new(@books).list_all_books
+  end
 end
