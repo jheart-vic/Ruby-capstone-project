9214f391c0584df988832c5e81edf22deff2facf
jheart-vic <adebowalevictorjheart@gmail.com>
2022-09-06 03:07:45 +0100

    Add book and Label classes

---
+++ book.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# the book class
+require './item'
+
+# the book class
+class Book < Item
+  attr_accessor :publisher
+
+  def initialize(id, publisher, cover_state)
+    super()
+    @id = id || Random.rand(1...1000)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archieved?
+    return true if super || cover_state == 'bad'
+
+    false
+  end
+end
---
+++ label.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# the Label class
+require './item'
+
+# the Label class
+class Label < Item
+  # the Label class
+  attr_accessor :title, :color
+  attr_reader :item
+
+  def initialize(id, title, color)
+    super()
+    @id = id || Random.rand(1...1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    Item.label = self
+  end
+end
