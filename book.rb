c6e6d01f2506281da266d1120f77dd116983c080
jheart-vic <adebowalevictorjheart@gmail.com>
2022-09-06 18:09:02 +0100

    Modify the book file

--- book.rb
+++ book.rb
@@ -6,10 +6,8 @@ class Book < Item
 
   def initialize(publisher, cover_state, publish_date, id = Random.rand(1...1000), archived: false)
     super(publish_date, id, archived: archived)
-    @id = id
     @publisher = publisher
     @cover_state = cover_state
-    @publish_date = publish_date
   end
 
   private
