68c426d605c41451add962b65144b9b0a1086caf
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-09-06 08:40:44 +0000

    create game class and inherit from item

---
+++ games/game.rb
@@ -0,0 +1,19 @@
+require '../item'
+class Game < Item
+    
+    def initialize(publish_date, multiplayer, last_played_at)
+        super(publish_date)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at ##date object: date_format: YYYY-mm-dd
+    end
+
+    private
+
+    def can_be_archived?
+        if @last_played_at.nil? 
+            return false
+        end
+        @last_played_at.next_year(2) > Date.today && super
+    end
+
+end
